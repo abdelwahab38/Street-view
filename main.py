8eadad5d91eae37f341b3caec0c060fdf68080a6
abdelwahab38 <60701350+abdelwahab38@users.noreply.github.com>
2024-07-29 16:07:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from streamlit_pannellum import streamlit_pannellum
 import streamlit as st
-st.image ("https://github.com/abdelwahab38/Street-view/edit/main/static/image_0023.jpg")
+
+st.image ("https://github.com/abdelwahab38/Street-view/edit/main/static/image_0023.jpg",caption="Sunrise by the mountains")
+
 streamlit_pannellum(
         config={
             "default": {
