0766ab70a038b85bcc46d5404d674a1784bdd7e7
abdelwahab38 <60701350+abdelwahab38@users.noreply.github.com>
2024-07-29 15:20:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,62 @@
-import os
 import streamlit as st
-st.write(os.path.dirname(__file__))
+from streamlit_pannellum import streamlit_pannellum
+import os
+
+path = os.path.dirname(__file__)
+
+image_0001_url = "/mount/src/street-view/static/image_0001.jpg"
+image_0002_url = "/mount/src/street-view/static/image_0002.jpg"
+
+streamlit_pannellum(
+        config={
+            "default": {
+                "firstScene": "first",
+            },
+            "scenes": {
+                "first": {
+                    "title": "My first example",
+                    "type": "equirectangular",
+                    "panorama": image_0001_url,
+                    "autoLoad": True,
+                    "author": "Me",
+                    "hotSpots": [
+                        {
+                            "pitch": 15,
+                            "yaw": 0,
+                            "type": "info",
+                            "text": "This is an info."
+                        },
+                        {
+                            "pitch": 0,
+                            "yaw": -10,
+                            "type": "scene",
+                            "text": "Second scene",
+                            "sceneId": "second"
+                        }
+                    ],
+                },
+                "second": {
+                    "title": "My second example",
+                    "type": "equirectangular",
+                    "panorama": image_0002_url,
+                    "autoLoad": True,
+                    "author": "always Me",
+                    "hotSpots": [
+                        {
+                            "pitch": 15,
+                            "yaw": 0,
+                            "type": "info",
+                            "text": "This is an info."
+                        },
+                        {
+                            "pitch": 0,
+                            "yaw": -10,
+                            "type": "scene",
+                            "text": "First scene",
+                            "sceneId": "first"
+                        }
+                    ],
+                }
+            }
+        }
+    )
