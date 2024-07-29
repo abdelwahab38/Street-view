de00a9ab613b1c3ff39f2b673d80e6a2abf4678b
abdelwahab38 <60701350+abdelwahab38@users.noreply.github.com>
2024-07-29 15:27:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,62 +1,9 @@
-import streamlit as st
-from streamlit_pannellum import streamlit_pannellum
 import os
-
-path = os.path.dirname(__file__)
-
-image_0001_url = "/mount/src/street-view/static/image_0001.jpg"
-image_0002_url = "/mount/src/street-view/static/image_0002.jpg"
-
-streamlit_pannellum(
-        config={
-            "default": {
-                "firstScene": "first",
-            },
-            "scenes": {
-                "first": {
-                    "title": "My first example",
-                    "type": "equirectangular",
-                    "panorama": image_0001_url,
-                    "autoLoad": True,
-                    "author": "Me",
-                    "hotSpots": [
-                        {
-                            "pitch": 15,
-                            "yaw": 0,
-                            "type": "info",
-                            "text": "This is an info."
-                        },
-                        {
-                            "pitch": 0,
-                            "yaw": -10,
-                            "type": "scene",
-                            "text": "Second scene",
-                            "sceneId": "second"
-                        }
-                    ],
-                },
-                "second": {
-                    "title": "My second example",
-                    "type": "equirectangular",
-                    "panorama": image_0002_url,
-                    "autoLoad": True,
-                    "author": "always Me",
-                    "hotSpots": [
-                        {
-                            "pitch": 15,
-                            "yaw": 0,
-                            "type": "info",
-                            "text": "This is an info."
-                        },
-                        {
-                            "pitch": 0,
-                            "yaw": -10,
-                            "type": "scene",
-                            "text": "First scene",
-                            "sceneId": "first"
-                        }
-                    ],
-                }
-            }
-        }
-    )
+import streamlit as st
+dossier = os.path.dirname(__file__)
+st.write (os.path.dirname(__file__))
+file_to_check = os.path.join(directory, 'static')
+if os.path.isdir(file_to_check) : 
+        st.write("YEEEEEEESSSSSSS...!!!!")
+else: 
+        st.write("NO !... :/")
