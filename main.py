a0db78223c3d6c3f34a9b3cdd9a7ba9e884949df
abdelwahab38 <60701350+abdelwahab38@users.noreply.github.com>
2024-07-29 16:32:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,38 @@
-from streamlit_pannellum import streamlit_pannellum
+import streamlit as st
+from streamlit_juxtapose import juxtapose
 
-streamlit_pannellum(
-    config={
-      "default": {
-        "firstScene": "first",
-      },
-      "scenes": {
-        "first": {
-          "title": "My first example",
-          "type": "equirectangular",
-          "panorama": "https://pannellum.org/images/alma.jpg",
-          "autoLoad": True,
-          "author": "Me",
-          "hotSpots": [
-            {
-              "pitch": 15,
-              "yaw": 0,
-              "type": "info",
-              "text": "This is an info."
-            },
-            {
-              "pitch": 0,
-              "yaw": -10,
-              "type": "scene",
-              "text": "Second scene",
-              "sceneId": "second"
-            }
-          ],
-        },
-        "second": {
-          "title": "My second example",
-          "type": "equirectangular",
-          "panorama": "/mount/src/street-view/static/alma.jpg",
-          "autoLoad": True,
-          "author": "always Me",
-          "hotSpots": [
-            {
-              "pitch": 15,
-              "yaw": 0,
-              "type": "info",
-              "text": "This is an info."
-            },
-            {
-              "pitch": 0,
-              "yaw": -10,
-              "type": "scene",
-              "text": "First scene",
-              "sceneId": "first"
-            }
-          ],
-        }
-      }
-    }
+from PIL import Image
+import requests
+
+import pathlib
+
+STREAMLIT_STATIC_PATH = (
+    pathlib.Path(st.__path__[0]) / "static"
+)  # at venv/lib/python3.9/site-packages/streamlit/static
+
+IMG1 = "image_0001.png"
+IMG2 = "image_0002.png"
+
+DEFAULT_IMG1_URL = (
+    "https://juxtapose.knightlab.com/static/img/Sochi_11April2005.jpg"
 )
+DEFAULT_IMG2_URL = (
+    "https://juxtapose.knightlab.com/static/img/Sochi_22Nov2013.jpg"
+)
+
+def fetch_img_from_url(url: str) -> Image:
+    img = Image.open(requests.get(url, stream=True).raw)
+    return img
+
+form = st.form(key="Image comparison")
+img1_url = form.text_input("Image one url", value=DEFAULT_IMG1_URL)
+img1 = fetch_img_from_url(img1_url)
+img1.save(STREAMLIT_STATIC_PATH / IMG1)
+
+img2_url = form.text_input("Image two url", value=DEFAULT_IMG2_URL)
+img2 = fetch_img_from_url(img2_url)
+img2.save(STREAMLIT_STATIC_PATH / IMG2)
+
+submit = form.form_submit_button("Submit")
+if submit:
+    juxtapose(IMG1, IMG2)
