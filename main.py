46ad15d4491df8016009a53496936a83c544dcb9
abdelwahab38 <60701350+abdelwahab38@users.noreply.github.com>
2024-07-29 15:28:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ if os.path.isdir(file_to_check) :
         st.write("YEEEEEEESSSSSSS...!!!!")
 else: 
         st.write("NO !... :/")
+
+items = os.listdir(file_to_check)
+for item in items : 
+        st.write(item)
