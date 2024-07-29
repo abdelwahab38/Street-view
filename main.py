52af9b89cccc05e7eb170380b93666ba33efd101
abdelwahab38 <60701350+abdelwahab38@users.noreply.github.com>
2024-07-29 15:27:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 dossier = os.path.dirname(__file__)
 st.write (os.path.dirname(__file__))
-file_to_check = os.path.join(directory, 'static')
+file_to_check = os.path.join(dossier, 'static')
 if os.path.isdir(file_to_check) : 
         st.write("YEEEEEEESSSSSSS...!!!!")
 else: 
